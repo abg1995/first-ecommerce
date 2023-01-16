4b1491ed835d54ae375ec2f515de8db20b468a1d
alejandro <aleblancogovantes@hotmail.com>
2023-01-16 18:08:51 +0100

    fixed chekout probelms: removed the .redirectToCheckout for: window.location.replace(data.url); on the cart component; changed line 41 on stripe.js from res.redirect(301).json... with: res.send({url: session.url}); succesfull send to checkout page and payment completion in stripe console logs.

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,26 +1,39 @@
 import React, { useRef } from "react";
 import Link from "next/link";
-import {
-  AiOutlineMinus,
-  AiOutlinePlus,
-  AiOutlineLeft,
-  AiOutlineShopping,
-} from "react-icons/ai";
+import {AiOutlineMinus,AiOutlinePlus, AiOutlineLeft,AiOutlineShopping} from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
 import { toast } from "react-hot-toast";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
+
+
 
 const Cart = () => {
   const cartRef = useRef();
-  const {
-    totalPrice,
-    totalQuantities,
-    cartItems,
-    setShowCart,
-    toggleCartItemQuantity,
-    onRemove
+  const {totalPrice,totalQuantities,cartItems,setShowCart,toggleCartItemQuantity,onRemove
   } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe =  await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading("Redirecting...");
+
+    window.location.replace(data.url);
+
+    }
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -89,7 +102,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
